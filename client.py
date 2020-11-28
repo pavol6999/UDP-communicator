04bb05d11f8a51fcca7fbcf90a8692a9c8931da7
Pavol Krajkovič <xkrajkovic@stuba.sk>
2020-11-28 16:04:09 +0100

    dorobeny klient

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import socket
 import struct
 import os
 
-
 from crcmod import crcmod
 
 
@@ -19,6 +18,7 @@ def ip_input():
 
     return ip_addr
 
+
 def port_input():
     port = int(input("Enter port: "))
     while True:
@@ -31,10 +31,10 @@ def port_input():
             port = int(input("Please enter a valid port nubmer: "))
     return port
 
+
 def initialize_connection(host_addr, host_port, frag_size, c_sock):
     i = 0
     init_packet = struct.pack(f"! c ", "I".encode())
-
 
     # klient sa pokusi 3x nadviazat spojenie, caka na taky isty packet co odoslal na server
     while i != 3:
@@ -47,12 +47,13 @@ def initialize_connection(host_addr, host_port, frag_size, c_sock):
                 break
         except socket.timeout:
 
-            print(f"Connection not initialized for {i+1}. time")
+            print(f"Connection not initialized for {i + 1}. time")
             i += 1
             if i == 3:
                 client_init()
                 return 0
     return 1
+
 
 def chop_data(byte_str, frag_size):
     fragments_q = []
@@ -73,16 +74,15 @@ def chop_data(byte_str, frag_size):
             end += frag_size
     return fragments_q
 
+
 def add_headers(chopped_bytes, data_type):
-
-
     i = 0
     fragments = []
 
     for data in chopped_bytes:
-        fragments.append(struct.pack("! c h", data_type.encode('ascii'), i+1) + data)
+        fragments.append(struct.pack("! c h", data_type.encode('ascii'), i + 1) + data)
         fragments[i] += struct.pack("! I", calculate_crc(fragments[i]))
-        i+=1
+        i += 1
     return fragments, i
 
 
@@ -90,7 +90,7 @@ def calculate_crc(fragment):
     return libscrc.fsc(fragment)
 
 
-def send_info_packet(host_addr,host_port, c_socket, data_type, frag_count, file_name):
+def send_info_packet(host_addr, host_port, c_socket, data_type, frag_count, file_name):
     i = 0
     if data_type == 'M':
         info_packet = struct.pack("!c h", data_type.encode('ascii'), frag_count)
@@ -102,14 +102,15 @@ def send_info_packet(host_addr,host_port, c_socket, data_type, frag_count, file_
 
     while i != 3:
         c_socket.settimeout(5)
-        c_socket.sendto(info_packet,(host_addr,host_port))
+        c_socket.sendto(info_packet, (host_addr, host_port))
         try:
             data, addr = c_socket.recvfrom(2048)
-            if struct.unpack("! c",data[0]) == 'A':
+            if data[0:1].decode('ascii') == 'A':
                 print(f"Bude odoslany subor {file_name}") if data_type == 'F' else print("Bude odoslana sprava")
+
                 break
         except socket.timeout:
-            i+=1
+            i += 1
             if i == 3:
                 print("Connection unstable")
                 client_init()
@@ -117,38 +118,79 @@ def send_info_packet(host_addr,host_port, c_socket, data_type, frag_count, file_
     return 1
 
 
-
-
 def transmit_data(host_addr, host_port, frag_size, data_type, c_socket):
-
     file_name = 0
 
     if data_type.upper() == "M":
-        data_type=data_type.upper()
+        data_type = data_type.upper()
         byte_str = input("Enter the message you want to send: ").encode('ascii')
 
-
     if data_type.upper() == "F":
-        data_type=data_type.upper()
+        data_type = data_type.upper()
         file_path = input_file_path()
         file_name = os.path.basename(file_path)
         print(f"File {file_name} is going to be send to the receiver")
         byte_str = open(file_path, "rb").read()
 
-    chopped_data = chop_data(byte_str,frag_size)
+    chopped_data = chop_data(byte_str, frag_size)
 
-    fragments,frag_count = add_headers(chopped_data, data_type)
-
+    fragments, frag_count = add_headers(chopped_data, data_type)
 
     init = initialize_connection(host_addr, host_port, frag_size, c_socket)
 
-
-    #ak sa podarilo ndviazat spojenie
+    # ak sa podarilo ndviazat spojenie
     if init != 0:
-        send_info_packet(host_addr, host_port, c_socket,data_type, frag_count,file_name)
+        send_info_packet(host_addr, host_port, c_socket, data_type, frag_count, file_name)
 
-    for fragment in fragments:
-        c_socket.sendto(fragment, (host_addr, host_port))
+    counter = 0
+    bulk_count = 0
+    left_to_send = frag_count
+    frag_indexes = []
+    index = 0
+
+    # while len(fragments) != 0:
+    #     while bulk_count < 9 and len(fragments) > 0:
+    #         fragment = fragments.pop(0)
+    #         c_socket.sendto(fragment, (host_addr, host_port))
+    #         frag_indexes.append((bulk_count,fragment))
+    #
+    #         bulk_count += 1
+    #     c_socket.settimeout(5)
+    #     data = c_socket.recvfrom(2048)
+    #     while True:
+    #         try:
+    #             data
+    #         except:
+    #             print("")
+
+    num_of_tries = 0
+
+    while len(fragments) != 0:
+        fragment = fragments.pop(0)
+
+        while num_of_tries != 3:
+            c_socket.settimeout(5)
+            c_socket.sendto(fragment, (host_addr, host_port))
+
+            try:
+                data, addr = c_socket.recvfrom(2048)
+
+                if data[0:1].decode('ascii') == 'A':
+                    if len(data) == 1:
+                        num_of_tries = 0
+                        c_socket.settimeout(None)
+                        break
+                    else:
+                        c_socket.sendto(fragment, (host_addr, host_port))
+
+
+            except socket.timeout:
+                num_of_tries += 1
+
+                if num_of_tries == 3:
+                    print("Connection unstable, disconnecting")
+                    client_init()
+                print(f"Retransmitting fragment for the {num_of_tries}. time")
 
 
 def input_file_path():
@@ -162,15 +204,13 @@ def input_file_path():
 
     return file_path
 
-def client_init():
 
+def client_init():
     host_addr = ip_input()
     host_port = port_input()
 
     frag_size = int(input("Fragment size: "))
     data_type = input("Message or File (m - f): ")  # TODO check data type
-
-
 
     c_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
--- server.py
+++ server.py
@@ -2,6 +2,11 @@ import socket
 import struct
 
 
+def unpack_data(fragment):
+    data_len = len(fragment) - 7
+    frag_type, frag_count, data, crc = struct.unpack(f"! c h {data_len}c I",fragment)
+    return frag_type,frag_count,data,crc
+
 def server_listen(port, s_socket):
     BUF_SIZE = 1500
 
@@ -10,23 +15,41 @@ def server_listen(port, s_socket):
     while True:
         print(f"######## SERVER IS LISTENING ON PORT {port} ########")
         s_socket.settimeout(100)
+        while True:
+            try:
 
-        try:
+                data, addr = s_socket.recvfrom(1500)
 
-            data, addr = s_socket.recvfrom(1500)
+                processed_packet_type = struct.unpack("! c", data[:1])[0]
+                if processed_packet_type.decode('ascii') == "I":
+                    print(f"Connection initialized by {addr[0]}")
+                    s_socket.sendto(data, addr)
 
-            processed_packet_type = struct.unpack("! c", data[:1])[0]
-            if processed_packet_type.decode('ascii') == "I":
-                print(f"Connection initialized by {addr[0]}")
-                s_socket.sendto(data, addr)
+                if processed_packet_type.decode('ascii') == 'M':
+                    frag_type = "M"
+                    frag_count = struct.unpack("!h",data[1:3])
+                    print(f"A message will be received consisting of {frag_count[0]} fragments")
+                    s_socket.sendto("A".encode('ascii'),addr)
+                    break
+
+                if processed_packet_type.decode('ascii') == 'F':
+                    frag_type = "F"
+                    frag_count = struct.unpack("!h",data[1:3])
+                    file_name = data[3:].decode('ascii')
+                    s_socket.sendto("A".encode('ascii'), addr)
+                    print(f"A file {file_name} will be received consisting of {frag_count[0]} fragments")
+                    break
+
+            except socket.timeout:
+                print("Connection unstable, closing")
+                return
 
 
-        except socket.timeout:
-            print("No client initialised a connection, closing port")
-            return
+        while True:
+            data, addr = s_socket.recvfrom(2048)
+            s_socket.sendto("A".encode('ascii'),addr)
+            print(data[3:-4].decode('ascii'))
 
-        data, addr = s_socket.recvfrom(1500)
-        print(f"{data}")
 
 
 def server_init():
